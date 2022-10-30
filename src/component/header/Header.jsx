f018ff74004662714b890465741155cf62eab175
unknown <oyinjoe23@gmail.com>
2022-10-30 10:21:34 +0100

    small changes

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "./Header.css"
 import ME from "../../assets/avatar3.jpg"
-import {BsShare} from "react-icons/bs"
+import {RiShareForwardLine} from "react-icons/ri"
 import {BsThreeDots} from "react-icons/bs"
 
 const Header = () => {
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="header__icons">
 
                 <div className="icon icon__1">
-                <BsShare />
+                <RiShareForwardLine />
                 </div>
 
                 <div className="icon icon__2" >
