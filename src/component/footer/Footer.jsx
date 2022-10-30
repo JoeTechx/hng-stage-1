a705d5a20e968132a04e807b2b0b36e71a4d25d2
unknown <oyinjoe23@gmail.com>
2022-10-30 11:35:17 +0100

    footer jsx

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 import "./Footer.css"
+import I4GLogo from "../../assets/I4G-Logo.png"
+import ZuriLogo from "../../assets/zurilogo.png"
+
 
 const Footer = () => {
+    
     return(
-        <div>
-            
-        </div>
+        <footer>
+            <hr/>
+            <img src= {ZuriLogo}alt="Zuri Logo"  className="zuri__logo"/>
+            <p className="footer__title">HNG Internship 9 Frontend Task</p>
+            <img src={I4GLogo} alt="I4G Logo" className="i4g__logo"/>
+
+        </footer>
 
     )
 }
