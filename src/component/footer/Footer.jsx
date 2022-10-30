01b56ab9ad22f172219475b543ffe84d78a11b76
unknown <oyinjoe23@gmail.com>
2022-10-30 12:32:28 +0100

    footer css

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -7,12 +7,13 @@ import ZuriLogo from "../../assets/zurilogo.png"
 const Footer = () => {
     
     return(
-        <footer>
+        <footer className="footer">
             <hr/>
+            <div className="footer__content"> 
             <img src= {ZuriLogo}alt="Zuri Logo"  className="zuri__logo"/>
             <p className="footer__title">HNG Internship 9 Frontend Task</p>
             <img src={I4GLogo} alt="I4G Logo" className="i4g__logo"/>
-
+            </div>
         </footer>
 
     )
