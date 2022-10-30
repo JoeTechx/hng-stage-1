818efeb8cd9374f46fba3330c3f34dca62c60463
unknown <oyinjoe23@gmail.com>
2022-10-30 09:51:17 +0100

    social

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from  "./component/header/Header"
 import Button from  "./component/button/Button"
+import Social from  "./component/social/Social"
 import Footer from  "./component/footer/Footer"
 import "./index.css"
 
@@ -9,6 +10,7 @@ const App = () => {
         <div className="link__tree">
              <Header/>
              <Button/>
+             <Social/>
              <Footer/>
         </div>
     )
---
+++ src/component/social/Social.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Social.css"
+import Slack from "../../assets/slack-new.png"
+import {BsGithub} from "react-icons/bs"
+
+
+const Social = () => {
+return(
+    <div className="social">
+
+    </div>
+)
+}
+
+export default Social
